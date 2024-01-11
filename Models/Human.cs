006bbf17b4960312d3469227e1eb4c4ef3a40034
Christoffer Jansson <christofferjansson1@gmail.com>
2024-01-11 19:21:33 +0100

    Add project files.

---
+++ Models/Human.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagHelpers.Models
+{
+    public class Human
+    {
+        [Required(ErrorMessage = "Everybody have a name")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "thath is not possible")]
+        public string Name { get; set; }
+        [Range(0, 120, ErrorMessage = "you cant be more than 120 or less than 0")]
+        public int Age { get; set; }
+
+        public bool Handsome { get; set; }
+    }
+}
